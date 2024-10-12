9f2ad5f0aed06d81f594f8cf45a8797b01c172fa
riskalaluna <riskalaluna@gmail.com>
2024-10-12 11:13:08 +0700

    when untuk mencetak nama hari berdasarkan nilai yang diberikan

--- app/src/main/java/com/example/questbasickotlin_0176/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/ConditionalExpression.kt
@@ -15,5 +15,17 @@ fun ConditionalStatement() {
         println("Negative Number")
     }
 
+    // When
+    val day = 1
+    when (day) {
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thursday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid Day")
+    }
 }
 
