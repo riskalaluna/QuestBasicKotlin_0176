7eb27e2154afddf91807a45fa34dd906884a9259
riskalaluna <riskalaluna@gmail.com>
2024-10-12 10:33:37 +0700

    membuat List Read-Only

---
+++ app/src/main/java/com/example/questbasickotlin_0176/Collections.kt
@@ -0,0 +1,14 @@
+package com.example.questbasickotlin_0176
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan memiliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// List Read Only menggunakan listOf
+// List Mutable menggunakan mutableListOf
+
+fun ContohList(){
+    println("==List==")
+    //List Read-Only
+    val readOnlyAbjad = listOf("A","B","C")
+    println(readOnlyAbjad)
+}
