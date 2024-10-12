74dfb6e1d02b9e177cb917ef4dfe2ef3e72dd347
riskalaluna <riskalaluna@gmail.com>
2024-10-12 10:56:50 +0700

    Menuliskan Map Mutable serta menambahkan, menghapus dan mengubah data dari Map Mutable

--- app/src/main/java/com/example/questbasickotlin_0176/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/Collections.kt
@@ -79,4 +79,20 @@ fun ContohMap() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    //Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    //Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
 }
