1d7b5f98b8daa0127fa13cec9acd79f0f7222f1b
riskalaluna <riskalaluna@gmail.com>
2024-10-12 11:09:12 +0700

    if-else untuk memeriksa apakah sebuah angka

--- app/src/main/java/com/example/questbasickotlin_0176/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/ConditionalExpression.kt
@@ -6,4 +6,14 @@ package com.example.questbasickotlin_0176
 
 fun ConditionalStatement() {
     println("== Conditional Expression ==")
+
+    // If-Else
+    val number = 10
+    if (number > 10) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
 }
+
