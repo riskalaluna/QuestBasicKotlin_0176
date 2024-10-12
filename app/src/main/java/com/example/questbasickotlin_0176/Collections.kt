69cc2becbe58ddb15a778151fd974bca70ab35eb
riskalaluna <riskalaluna@gmail.com>
2024-10-12 10:35:37 +0700

    membuat List Mutable

--- app/src/main/java/com/example/questbasickotlin_0176/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/Collections.kt
@@ -11,4 +11,9 @@ fun ContohList(){
     //List Read-Only
     val readOnlyAbjad = listOf("A","B","C")
     println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle","Square","Triangle")
+    println(shape)
+
 }
