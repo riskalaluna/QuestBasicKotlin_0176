faff5608765e2a0497269a03717ea25a0af96e54
riskalaluna <riskalaluna@gmail.com>
2024-10-12 11:05:51 +0700

    Mendefinisikan fungsi dari ConditionalStatement

---
+++ app/src/main/java/com/example/questbasickotlin_0176/ConditionalExpression.kt
@@ -0,0 +1,9 @@
+package com.example.questbasickotlin_0176
+
+// Conditional Expression
+// Conditional Expression adalah Ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("== Conditional Expression ==")
+}
