14aebf64d8e42848c46512b76215f418a0f822cf
riskalaluna <riskalaluna@gmail.com>
2024-10-13 12:08:55 +0700

    menuliskan fun nullSafety

--- app/src/main/java/com/example/questbasickotlin_0176/NullSafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/NullSafety.kt
@@ -1,2 +1,14 @@
 package com.example.questbasickotlin_0176
 
+fun nullSafety() {
+//neverNull has String type
+    var neverNull: String = "This can't be null"
+
+//Throws a compiler error
+//neverNull = null
+
+//nullable has nullable String type
+    var nullable: String? = "You can keep a null her e"
+//This is OK
+    nullable = null
+}
