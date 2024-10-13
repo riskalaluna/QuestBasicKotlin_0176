609d7b5e3654263b842174668ecc985c4173b122
riskalaluna <riskalaluna@gmail.com>
2024-10-13 11:54:59 +0700

    Menuliskan functions dengan default Parameter

--- app/src/main/java/com/example/questbasickotlin_0176/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/Functions.kt
@@ -10,3 +10,31 @@ fun withParameter(name: String){
     println("== withParameter ==")
     println("Hello, $name!=")
 }
+
+//named argument
+//named argument merupakan cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int){
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+//default parameter value
+//default parameter merupakan cara untuk memberikan  nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int){
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old")
+}
+
+fun withReturn(panjang : Int, lebar : Int) : Int {
+    return (panjang * lebar)
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+
+}
