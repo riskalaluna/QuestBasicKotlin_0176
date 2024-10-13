f232195048087ff1fc4355a15fb5f0ef18b84d24
riskalaluna <riskalaluna@gmail.com>
2024-10-13 11:41:12 +0700

    Menuliskan isi DataClass dan menuliskan fungsi Equals dan fungsi copy

--- app/src/main/java/com/example/questbasickotlin_0176/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/DataClass.kt
@@ -5,3 +5,24 @@ package com.example.questbasickotlin_0176
 // Data Class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString()
 // Data Class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
 
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+M
+fun main () {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+//Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+
+// Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+// Fungsi Copy dengan perubahan
+    val data4 = data.copy( email = "sahar@gmail.com")
+    println(data4)
+}
