8c8285e9f1ba90edc852faf375e92f9366dc1f4f
riskalaluna <riskalaluna@gmail.com>
2024-10-13 12:06:02 +0700

    menuliskan codingan di file Lamda Functions (fungsi yang tidak memiliki nama)

--- app/src/main/java/com/example/questbasickotlin_0176/LambdaFunction.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/LambdaFunction.kt
@@ -1,2 +1,17 @@
 package com.example.questbasickotlin_0176
 
+//Lambda Functions
+//Lambda Functions adalah fungsi yang tidak memiliki nama
+//Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+//Lambda Functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+//Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))
+// HELLO
+}
