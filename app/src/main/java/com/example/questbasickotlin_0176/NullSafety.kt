d2c1c6f52f5bde36efa4b9e20ad5021553e25670
riskalaluna <riskalaluna@gmail.com>
2024-10-13 12:13:37 +0700

    menuliskan fun nullSafety

--- app/src/main/java/com/example/questbasickotlin_0176/NullSafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0176/NullSafety.kt
@@ -11,4 +11,28 @@ fun nullSafety() {
     var nullable: String? = "You can keep a null her e"
 //This is OK
     nullable = null
+
+// Check for null in conditions
+if(neverNull == null) {
+    println("inferredNonNull is null")
+} else {
+    println("inferredNonNull is not null")
+}
+
+// safe call operator
+println(neverNull.length) //18
+println(nullable?.length) // null
+
+
+//notNull doesnt accept null value
+fun strLength(notNull: String): Int{
+    return notNull.length
+}
+
+println(strLength(neverNull)) //18
+
+}
+
+fun main() {
+    nullSafety()
 }
