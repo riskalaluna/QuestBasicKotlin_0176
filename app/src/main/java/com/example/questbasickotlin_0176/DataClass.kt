ad06168c4f949c60dc0cddf8b329f69caea9f941
riskalaluna <riskalaluna@gmail.com>
2024-10-13 11:33:07 +0700

    membuat file DataClass dan menuliskan pengertian Data Class

---
+++ app/src/main/java/com/example/questbasickotlin_0176/DataClass.kt
@@ -0,0 +1,7 @@
+package com.example.questbasickotlin_0176
+
+// Data Class
+// Data Class adalah class yang digunakan untuk menyimpan data
+// Data Class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString()
+// Data Class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan
+
